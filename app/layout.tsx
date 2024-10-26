2cfeefe4f2de21631fd83f60b1f8d5758ac7c36c
Rafael "Caslu" Henry Cassiano Luiz <147253148+RCaslu@users.noreply.github.com>
2024-10-26 09:11:32 -0300

    Test: testing max-w-[1366px] on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="max-w-screen-auto mx-auto">
+        <div className="max-w-[1366px] mx-auto">
           {children}
         </div>
         <Footer />
