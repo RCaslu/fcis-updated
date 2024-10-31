e47722351a5046569a06bd11a7c2a2e9f058ef2a
Rafael Caslu Henry Cassiano Luiz <147253148+RCaslu@users.noreply.github.com>
2024-10-31 19:29:41 +0000

    fix: fixed mobile forms

--- app/_components/forms.tsx
+++ app/_components/forms.tsx
@@ -67,8 +67,8 @@ export default function Form({ device }: FormProps) {
   return (
     <div className='flex flex-col text-center item-center font-zillaslab'>
         <div className=' py-12 '>
-        <form onSubmit={handleSubmit} className='py-[5px] rounded-xl bg-white'>
-        <p className='text-black py-6 pt-12'>Um de nossos consultores vai entrar em contacto consigo</p>
+        <form onSubmit={handleSubmit} className='py-[5px] rounded-xl bg-white w-[420px] mx-auto'>
+        <strong><p className='text-black py-6 pt-12'>Um de nossos consultores vai entrar em contacto consigo</p></strong>
       <div className='py-2'>
                     <input 
                         type="name" 
@@ -109,6 +109,9 @@ export default function Form({ device }: FormProps) {
           </div>
         )}
         <Button 
+        style={{
+          fontFamily: 'Poppins, sans-serif',
+        }}
         type="submit"
         className=""
         variant={'ProjectButtonForms'}
@@ -117,7 +120,7 @@ export default function Form({ device }: FormProps) {
           <br />TRANSFORMADA
         </Button>
         <div>
-            <p className='text-black pb-12'>Preenche o formulário acima e <span className='font-bold'>tenha a vida transformada</span></p>
+            <strong><br/><p className='text-black pb-12'>Preenche o formulário acima e <span className='font-bold'>tenha a vida transformada</span></p></strong>
         </div>
       </form>
         </div>
