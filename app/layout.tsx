242521553622eefd568cb9318e4359b088b02521
Rafael Caslu Henry Cassiano Luiz <147253148+RCaslu@users.noreply.github.com>
2024-11-02 18:36:02 +0000

    fix: fixed text-white on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="max-w-auto mx-auto overflow-hidden">
+        <div className="max-w-auto mx-auto overflow-hidden text-white">
           {children}
         </div>
         <Footer />
